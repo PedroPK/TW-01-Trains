04d09790396a939b93d62e87e195a1f3c91d8bf3
Pedro <pedropk@gmail.com>
2018-07-10 18:51:26 -0300

    Implemented the first Vertex Tests

--- TW 01 - Trains/tsw/tw/graph/VertexTest.java
+++ TW 01 - Trains/tsw/tw/graph/VertexTest.java
@@ -1,12 +1,49 @@
 package tw.graph;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class VertexTest {
 	
 	@Test
-	public void test() {
+	public void testConstructorNullString() {
+		String input = null;
 		
+		Vertex vertex = new Vertex(input);
+		
+		assertNotNull(			vertex);
+		assertNull(				vertex.getName());
+		assertNull(				vertex.getNextVertex());
+		assertEquals(	0,		vertex.getDistanceNextVertex());
+	}
+	
+	@Test
+	public void testConstructorEmptyString() {
+		String input = "";
+		
+		Vertex vertex = new Vertex(input);
+		
+		assertNotNull(			vertex);
+		assertNull(				vertex.getName());
+		assertNull(				vertex.getNextVertex());
+		assertEquals(	0,		vertex.getDistanceNextVertex());
+	}
+	
+	@Test
+	public void testConstructorBasicPath() {
+		String input = "AB5";
+		
+		Vertex vertex = new Vertex(input);
+		
+		assertNotNull(			vertex);
+		assertNotNull(			vertex.getName());
+		assertNotNull(			vertex.getNextVertex());
+		assertEquals(	5,		vertex.getDistanceNextVertex());
+		
+		assertEquals(	"A",	vertex.getName());
+		assertEquals(	"B",	vertex.getNextVertex().getName());
+		assertNull(				vertex.getNextVertex().getNextVertex());
+		assertEquals(	0,		vertex.getNextVertex().getDistanceNextVertex());
 	}
 	
 }
